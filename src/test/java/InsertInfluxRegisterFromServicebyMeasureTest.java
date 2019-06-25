9366e7dac619b6e0f0073d4711e2b89ff1063627
jimenezict <jimenezict@gmail.com>
2019-06-25 22:36:27 +0200

    Adding exception control on main scheduler

---
+++ src/test/java/InsertInfluxRegisterFromServicebyMeasureTest.java
@@ -0,0 +1,32 @@
+import com.jimenezict.bicing.getstationstatus.influx.InfluxRegisterDAO;
+import com.jimenezict.bicing.getstationstatus.influx.InfluxRegisterDAOImpl;
+import com.jimenezict.bicing.getstationstatus.influx.dto.InfluxRegisterDTO;
+import com.jimenezict.bicing.getstationstatus.service.InsertToDatabase;
+import com.jimenezict.bicing.getstationstatus.service.InsertToDatabaseImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+public class InsertInfluxRegisterFromServicebyMeasureTest {
+
+    private InsertToDatabase insertToDatabase;
+
+    @Before
+    public void init(){
+        insertToDatabase = new InsertToDatabaseImpl();
+    }
+
+    @Test
+    public void insertRegisterOfAvailabilityOnFirstStationOnInfluxRegisterDAO(){
+        List<InfluxRegisterDTO> influxRegisterDTOS = new ArrayList<>();
+        influxRegisterDTOS.add( new InfluxRegisterDTO(1000,15.0));
+        insertToDatabase.insertListOfStationsToGetNumberOfAvailableDocks(influxRegisterDTOS);
+    }
+
+}
