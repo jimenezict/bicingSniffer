c5d81084d675084a73ac1dbe4a14a3f48d1f754e
jimenezict <jimenezict@gmail.com>
2019-09-01 18:25:37 +0200

    Refactoring Test Name to include the integrafion suffix

--- src/test/java/BicingApiCallImplIT.java
+++ src/test/java/BicingApiCallImplIT.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-public class BicingApiCallTest {
+public class BicingApiCallImplIT {
 
     private BicingApiCall underTest;
 
