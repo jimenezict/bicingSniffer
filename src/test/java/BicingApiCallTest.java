8c8ad2925051e38fb06c0af0d07594e672672181
jimenezict <jimenezict@gmail.com>
2019-09-01 17:46:52 +0200

    Renaming of the the test to the BSM API

--- src/test/java/BicingApiCallTest.java
+++ src/test/java/BicingApiCallTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-public class GetStationStatusTest {
+public class BicingApiCallTest {
 
     private BicingApiCall bicingApiCall;
 
